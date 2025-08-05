b4303bb335d7b74e9f944679321d892af30d2d38
Azhar Mahmud <azharmahmud730@gmail.com>
2025-08-05 06:31:12 +0600

    specialties

--- src/app/modules/User/user.controller.ts
+++ src/app/modules/User/user.controller.ts
@@ -4,6 +4,7 @@ import sendResponse from "../../../shared/sendResponse";
 import httpStatus from "http-status";
 import pick from "../../../shared/pick";
 import { userFilterableFields } from "./user.constant";
+import { IAuthUser } from "../../interfaces/common";
 
 const createAdmin = catchAsync(async (req, res) => {
   const result = await UserServices.createAdminIntoDB(req);
@@ -61,11 +62,39 @@ const changeProfileStatus = catchAsync(async (req, res) => {
     data: result,
   });
 });
+const getMyProfile = catchAsync(async (req, res) => {
 
+  const user = req.user;
+
+  const result = await UserServices.getMyProfile(user as IAuthUser);
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: "My profile data fetched!",
+    data: result
+  })
+});
+
+const updateMyProfie = catchAsync(async (req, res) => {
+
+  const user = req.user;
+
+  const result = await UserServices.updateMyProfie(user as IAuthUser, req);
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: "My profile updated!",
+    data: result
+  })
+});
 export const UserControllers = {
   createAdmin,
   createDoctor,
   createPatient,
   getAllUser,
   changeProfileStatus,
+  getMyProfile,
+  updateMyProfie
 };
--- src/app/modules/User/user.route.ts
+++ src/app/modules/User/user.route.ts
@@ -13,7 +13,11 @@ router.get(
   auth(UserRole.SUPER_ADMIN, UserRole.ADMIN),
   UserControllers.getAllUser
 );
-
+router.get(
+  '/me',
+  auth(UserRole.SUPER_ADMIN, UserRole.ADMIN, UserRole.DOCTOR, UserRole.PATIENT),
+  UserControllers.getMyProfile
+)
 router.post(
   "/create-admin",
   auth(UserRole.SUPER_ADMIN, UserRole.ADMIN),
@@ -50,6 +54,16 @@ router.patch(
   auth(UserRole.SUPER_ADMIN, UserRole.ADMIN),
   validateRequest(UserValidation.updateStatus),
   UserControllers.changeProfileStatus
+);
+
+router.patch(
+  "/update-my-profile",
+  auth(UserRole.SUPER_ADMIN, UserRole.ADMIN, UserRole.DOCTOR, UserRole.PATIENT),
+  fileUploader.upload.single('file'),
+  (req: Request, res: Response, next: NextFunction) => {
+    req.body = JSON.parse(req.body.data)
+    return UserControllers.updateMyProfie(req, res, next)
+  }
 );
 
 export const UserRoutes = router;
